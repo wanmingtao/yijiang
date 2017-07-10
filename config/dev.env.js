c98492c130b85c27c834086922e5d204641c662a
DESKTOP-CRAKVN0\wan <2268962378@qq.com>
2017-07-11 07:41:02 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  proxyTable:{'/tp/public/api1':{target:'http://localhost',changeOrigin: true}}
+  proxyTable:{'/tp/public/api2':{target:'http://10.2.153.118',changeOrigin: true}}
 })
